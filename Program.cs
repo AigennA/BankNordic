f225e1dcbd832b564891019674be754661850827
Aigen80 <aomurm@gmail.com>
2025-09-18 15:13:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankNordic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
