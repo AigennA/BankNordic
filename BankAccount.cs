32d4d730b139c3f37e7df6f611bdc4a9edf9ed48
Aigen80 <aomurm@gmail.com>
2025-09-19 13:43:01 +0200

    Added BancAccount class method to deposit money

--- BankAccount.cs
+++ BankAccount.cs
@@ -3,7 +3,7 @@
     private decimal balance;
     public decimal Balance => balance;
 
-    public void Deposit(decimal amount)
+    public void Deposit(decimal amount) // Method to deposit money
     {
         if (amount > 0)
             balance += amount;
